fbf84e49c37b47ddf195d790339d1877e8311596
jonathan <joniduran@gmail.com>
2021-03-27 22:09:56 -0300

    Leyendo documentos de firestores p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import {firebase} from './firebase'
 
 function App() {
+
+
+    React.useEffect(()=>{
+
+      const obtenerDatos = async () =>{
+        const db = firebase.firestore()
+        const data = await db.collection('tareas').get()
+        
+        try {
+          
+        } catch (error) {
+          console.log(error)  
+        }
+      }
+
+      obtenerDatos()
+    },[])
 
     return (
     <div className="container mt-3">
